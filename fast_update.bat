3f709b8a76aba72f82ec9a0da7fe01fe7d601a06
Topaz <94007929+TopazZD@users.noreply.github.com>
2024-09-02 08:20:55 -0400

    Skipped updating

--- fast_update.bat
+++ fast_update.bat
@@ -5,7 +5,6 @@ git pull > NUL
 
 if not exist TerraAngelSetup\TerraAngelSetup\bin\Release\net7.0\TerraAngelSetup.exe (
     echo Building TerraAngelSetup
-    git submodule update --remote --recursive > NUL
     dotnet build TerraAngelSetup\TerraAngelSetup\TerraAngelSetup.csproj -c=Release > NUL
 )
 
